95448ac7c66f11a60bdeb94fbacceaa38c5a2979
Your NameEfe-Claire <efeclaire.karieren@gmail.com>
2023-03-01 11:38:09 +0000

    Introduction to PHP

---
+++ Lab 6/Index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	</head>
+	<body>
+        <p>
+          <?php
+          echo "Hello, World";
+          echo "Hello," . " " . "world" . "!";
+          echo 5 * 7;
+          $myname = "Bilbo Baggins";
+          $myage = 111;
+          echo "My name is" . $myname . "and I am " . $myage;
+    echo "I get printed!";
+    // I don't! I'm a comment.
+    /* I don’t get printed either
+   and neither do I */
+   $name = "Edgar";
+
+   if ($name == "Edgar") {
+     print "I know you!";
+   }
+   else {
+    print "Who are you?";
+}
+          ?>
+        </p>
+	</body>
+</html
