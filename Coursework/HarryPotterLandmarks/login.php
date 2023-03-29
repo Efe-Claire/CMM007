22da8a163dc96491d42e0e22c274662c1923c5c7
Efe-Claire <efeclaire.karieren@gmail.com>
2023-03-29 18:43:33 +0100

    Added an Image folder

--- Coursework/HarryPotterLandmarks/login.php
+++ Coursework/HarryPotterLandmarks/login.php
@@ -8,21 +8,20 @@
 	<div class="login-box">
 		<h2>Login</h2>
 		<form action="auth.php" method="POST">
-        <div class="user-type">
-				<span>Are you a user or admin?</span>
-				<select name="user-type" required><br></br>
-					<option value="">Select an option</option>
-					<option value="user">User</option>
-					<option value="admin">Admin</option>
-				</select><br></br>
   <label for="username">Username:</label>
   <input type="text" id="username" name="username" placeholder="Enter your username" required><br></br>
   <label for="password">Password:</label>
   <input type="password" id="password" name="password" placeholder="Enter your password" required><br></br>
   <input type="submit" value="Login">
 </form>
-
-		<p>Don't have an account? <a href="signup.php">Sign up</a></p>
+	<?php
+include("connection.php"); //Establishing connection with our database
+if(empty($_POST["username"]) || empty($_POST["password"]))
+ {
+ echo "Both fields are required.";
+ }else
+ {
+	<p>Don't have an account? <a href="signup.php">Sign up</a></p>
 	</div>
 </body>
 </html>
